7f0e210df7792ecb2074e28a8c464ac08c1203f5
Taelo <tseholoba2@gmail.com>
2021-01-15 15:29:48 +0200

    This is the update of Task05

--- Task05.java
+++ Task05.java
@@ -3,12 +3,12 @@
   public class Task05 {
       public static double AreaOfTriangle(double side1, double side2, double side3){
       double triangleSides = 0.5*(side1 + side2 + side3);
-      double triangleArea = Math.sqrt(triangleSides * ((triangleSides - side1 ) + (triangleSides - side2) + (triangleSides - side3)));
+      double triangleArea = Math.sqrt(triangleSides * ((triangleSides - side1 ) * (triangleSides - side2) *(triangleSides - side3)));
  
   return triangleArea;
   }
   public static void main(String[] args) {
-     System.out.print(AreaOfTriangle(3,4,5));
+     System.out.print(AreaOfTriangle(6,5,5));
   }
 
 }
