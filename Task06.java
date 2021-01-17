82f78d47ca31de2ae768b18879cddc2f00c1c008
Taelo <tseholoba2@gmail.com>
2021-01-17 22:19:04 +0200

    This is the update of Task06

--- Task06.java
+++ Task06.java
@@ -2,11 +2,11 @@ package org.umuzi;
 
 public class Task06 {
   public static int maximum(int num1,int num2 , int num3){
-    if(num1 > num2 && num1 > num3)
+    if(num1 >= num2 && num1 >= num3)
           return num1;
-        if(num2 > num1 && num2 > num3)
+        if(num2 >= num1 && num2 >= num3)
           return num2;
-        else
+        else 
           return num3;
         
       
@@ -14,7 +14,7 @@ public class Task06 {
   
 
   public static void main(String[] args) {
-    System.out.println(maximum(51,3,71));
+    System.out.println(maximum(9,2,6));
   }
 
 }
